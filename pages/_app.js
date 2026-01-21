a372d4f6404f2f3896e89d070d597cf855c441b4
ChuongBang <chuongbn@softdreams.vn>
2026-01-21 08:23:35 +0700

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Dragon Match Tracker</title>
+        <meta name="description" content="Quản lý trận đấu" />
+        <link rel="icon" type="image/png" href="/bmt_favicon.png" />
+        <link rel="shortcut icon" href="/bmt_favicon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
